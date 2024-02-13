b383b885919852e24e3b0eb3f958441bf40a4f18
prashanth <prashanthseven@gmail.com>
2024-02-14 00:38:29 +0530

    youtube.js library

--- reacttestapp/reacttestapp/src/App.js
+++ reacttestapp/reacttestapp/src/App.js
@@ -1,45 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import Row from './row';
 import requests from './requests';
 import movieTrailer from "movie-trailer";
-import axios from './axios';
-import { React, useState, useEffect } from 'react'
-import YouTube from "react-youtube-test";
-
-
-
+import { React, useState } from 'react'
 
 function App() {
-  const [clickedRowId, setClickedRowId] = useState(0);
-  const [trailerUrl, setTrailerUrl] = useState("");
+    const [clickedRowId, setClickedRowId] = useState(0);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
-
-  const handleRowClick = (newRowId, movieId) => {
-    movieTrailer(null, {tmdbId: movieId})
-    .then((url) => {
-        // everything after the question mark
-        const urlParams = new URLSearchParams(new URL(url).search);
-        const ytFragment = urlParams.get('v');
-        console.log("========setting trailer url to " + ytFragment)
-        setTrailerUrl(ytFragment);
-        setClickedRowId(newRowId);
-    }).catch(error => console.log(error + " " + movieId));
-    // setTrailerUrl(url)
-  }
-  const opts = {
-        height: "300",
-        width: "100%",
-        playerVars: {
-            autoplay: 1,
-            mute: 1,
-            loop: 1,
-        }
+    const handleRowClick = (newRowId, movieId) => {
+        movieTrailer(null, {tmdbId: movieId})
+        .then((url) => {
+            // everything after the question mark
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'));
+            setClickedRowId(newRowId);
+        }).catch(error => console.log(error + " " + movieId));
     }
 
   return (
     <div className="app">
-      {/* <YouTube videoId={"85Sg3MTrXyo"} opts={opts}/> */}
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} rowId={1} clickedRowId={clickedRowId} handleRowClick={handleRowClick} trailerUrl={trailerUrl}/>
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} rowId={2} clickedRowId={clickedRowId} handleRowClick={handleRowClick} trailerUrl={trailerUrl}/>
     </div>
--- reacttestapp/reacttestapp/src/row.js
+++ reacttestapp/reacttestapp/src/row.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react'
 import './row.css';
-import YouTube from "react-youtube-test";
+import YouTube from "./youtube";
 import axios from './axios';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
@@ -18,20 +18,9 @@ function Row({ title, fetchUrl, rowId, clickedRowId, handleRowClick, trailerUrl 
         fetchData();
     }, [fetchUrl]);
 
-    const opts = {
-        height: "390",
-        width: "100%",
-        playerVars: {
-            autoplay: 1,
-            mute: 1,
-            loop: 1,
-        }
-    }
-
     return (
         <div className="row">
             <h2>{title}</h2>
-            {/* <h1 className="title">This is a row in the test app.</h1> */}
             <div className="row__posters">
                 {movies.map(movie => {
                     return <img
@@ -43,18 +32,7 @@ function Row({ title, fetchUrl, rowId, clickedRowId, handleRowClick, trailerUrl 
                     />
                 })}    
             </div>
-            {/* <button className="row__button" onClick={() => { handleRowClick(rowId, movieId) }}>Click here</button> */}
-            {/* {<YouTube videoId={trailerUrl} opts={opts}/>} */}
-            {clickedRowId === rowId && <iframe
-                src={`https://www.youtube-nocookie.com/embed/${trailerUrl}?autoplay=1&mute=1&enablejsapi=1`}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen={true}
-                height="390"
-                width="100%"
-                frameBorder={0}
-            />}
-
+            {clickedRowId === rowId && <YouTube url={trailerUrl} height="390" width="100%"/>}
         </div>
     )
 }
